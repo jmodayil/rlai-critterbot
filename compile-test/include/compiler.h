3bab030ca995ecc5209a8ba3ff6beca22f5ca667
marcgb <marcgb@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-07-03 17:52:34 +0000

    Fixed serialecho.c to have some includes, changed makefile to produce a binary

---
+++ compile-test/include/compiler.h
@@ -0,0 +1,22 @@
+/*
+ *
+ * os.h
+ *
+ * Created by Marc G. Bellemare on 1/04/08
+ *
+ * Define copmiler-specific flags
+ */
+#ifndef OS_H
+#define OS_H
+
+#ifdef __GNUC__
+// Necessary to avoid duplicate definitions, behavior unknown
+#define __inline static inline
+#define FL_RAMFUNC
+#define FL_ARM
+#else
+#error Unsupported compiler! Contact the authors.
+#endif
+
+#endif /* OS_H */
+
