672df0f96fee828f04e994d7891f3112965eab3e
mike.sokolsky <mike.sokolsky@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-07-04 20:22:42 +0000

    Real armio.c, needs a little work still

--- trunk/libs/armio.h
+++ trunk/libs/armio.h
@@ -5,10 +5,21 @@
  *
  */
 
+#ifndef ARMIO_H
+#define ARMIO_H
+
+#include "AT91SAM7S256.h"
+#include "lib_AT91SAM7S256.h"
+
 #define MAX_INT_PRINT_SIZE 32
 #define SIGNED 1
 #define UNSIGNED 0
-#define SER_BAUD_CD (MCK / (16 * SER_BAUD_RATE))
+#define SER_BRGR (MCK / 16 / SER_BAUD_RATE)
+
+#define SERIAL_A_PINS (AT91C_PA5_RXD0 | AT91C_PA6_TXD0)
+#define SERIAL_B_PINS 0
+
+#define SER_BAUD_RATE 230400
 #define SER_TX_BUF_SIZE 1024
 #define SER_RX_BUF_SIZE 1024
 
@@ -64,15 +75,43 @@ void armprintf(char*, ...);
 int armsscanf(char*, char*, ...);
 
 void itoa(int, char*, int, int );
+int atoi(char*, int*);
 void strrev(char*);
+__inline int getvalue(char);
 
 /*
  * Interrupt routine for serial port.
  * Must be run in RAM.
  */ 
-RAM_FUNCTION void ser_isr(void);
+ARM_CODE RAM_FUNCTION void ser_isr(void);
 
 /*
  * Initialize the serail port for character stream input/output
  */
 void init_serial_port_stdio(void);
+
+/*
+ * Send a character out the serial port
+ */
+void armputchar(char);
+
+/*
+ * Get a character from the serial port
+ *
+ * returns character, or EOF if no data in buffer
+ */
+int armgetchar(void);
+
+/*
+ * Reads a newline terminated string from the serial port.
+ *
+ * Takes a pointer to the string buffer and the size of the buffer
+ * 
+ * Non-blocking, returns EOF if there is no full string in the port.
+ * Otherwise returns number of characters read
+ *
+ */
+int armreadline(char*, int);
+
+#endif
+
