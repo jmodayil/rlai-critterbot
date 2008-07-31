65fbe03b60c8ed2aa761800a0071e942edbbfe22
marcgb <marcgb@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-07-31 16:43:10 +0000

    Added semaphores to lib_critical (test and release)

--- trunk/libs/lib_critical.h
+++ trunk/libs/lib_critical.h
@@ -17,9 +17,25 @@ void crit_enable_int();
 /** Disable interrupts */
 void crit_disable_int();
 
-/** Set a semaphore */
-void crit_set_mutex(crit_sem * semaphore);
-/** Clears a semaphore */
-void crit_clear_mutex(crit_sem * semaphore);
+/**
+  * Tests whether a semaphore is being used. This routine is provided for
+  *  interrupts to avoid blocking in one.
+  * Returns non-zero if the semaphore is held by another process.
+  */
+int crit_test_mutex(crit_sem * semaphore);
+/** 
+  * Requests a semaphore (and block until the semaphore is free).
+  *  Two threads wishing to synchronize or write to the same data should 
+  *  use a volatile crit_sem and call crit_get_mutex on this semaphore
+  *  before accessing the data.
+  */
+void crit_get_mutex(crit_sem * semaphore);
+/** 
+  * Releases a semaphore. For simplicity, the semaphore does not contain 
+  *  information as to whether the same 'thread' who got the semaphore is
+  *  trying to release it. In other words, proper use of semaphores requires
+  *  that the semaphore be released only after getting it.
+  */
+void crit_release_mutex(crit_sem * semaphore);
 
 #endif /* LIB_CRITICAL_H */
