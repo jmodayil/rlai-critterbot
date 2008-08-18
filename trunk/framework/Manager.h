14cdb305adac2e0d955ba41c89c2be790e2bb995
anna.koop <anna.koop@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-08-18 21:51:16 +0000

    Initial pass at a manager file for handling robot drops

---
+++ trunk/framework/Manager.h
@@ -0,0 +1,30 @@
+#ifndef _H
+#define Manager_H
+
+#include "Component.h"
+
+/**
+ * @todo: comment me
+ */
+class Manager : public Component {
+  public:
+    Manager(DataLake *lake, ComponentConfig &config, string &name);
+    virtual ~Manager();
+
+    //for reloading config when it changes
+    virtual int loadConfig(ComponentConfig *config) ;
+
+    //initial the device. does not block, will be called multiple times
+    //if nessecary. return 0 if not done, negative on error, and positive when done
+    virtual int init(USeconds &wokeAt);
+
+    //read data, transform data, write new data
+    virtual int think(USeconds &wokeAt);
+
+
+ private:
+		RiverWrite controlOutput;
+		RiverRead joystickInput;
+};
+
+#endif
