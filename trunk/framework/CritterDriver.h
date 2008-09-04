ef81c83d082ff8324d80e3f540d1d17d51c11ff5
jpnebox <jpnebox@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-09-04 21:48:58 +0000

    CritterDriver compiles

--- trunk/framework/CritterDriver.h
+++ trunk/framework/CritterDriver.h
@@ -1,11 +1,16 @@
 #ifndef CritterDriver_H
 #define CritterDriver_H
 
+#define BAUDRATE B115200
+#define DEVICE "/dev/ttyS0"
+
+#include <termios.h>
+
 #include "Component.h"
 #include "BitMask.h"
 #include "ErrorMessage.h"
-#include "CritrterControlDrop.h"
-#include "CritrterStateDrop.h"
+#include "CritterControlDrop.h"
+#include "CritterStateDrop.h"
 
 using namespace std;
 
@@ -18,48 +23,31 @@ class CritterDriver : public Component {
     CritterControlDrop* controlDrop;
     CritterStateDrop stateDrop;
 
-    boolean newData;
+    bool newData;
 
     int thinks;
     
-    int postWait;
-    USeconds lastPost;
+    struct termios oldterm;
     int fid;
     string device;
     int port;
 
   public:
     
-    CritterDriver();
+    CritterDriver(DataLake *lake, ComponentConfig &conf, string &name);
     virtual ~CritterDriver();
 
-    //returns the fid of the io device. If no fid exists,
-    //will return fileno(stdout). stdout is never ready for reading.
-    //return fileno(stdout) means poll me, i'm not event driven.
     virtual int getFID();
-
-    //reads from the device to DataLake. if the fid exists, receive() will
-    //only be called when data is present
-    //if the device wishes to be closed, return a negative number
-    virtual int sense(USeconds &wokeAt);
-
-    //initial the device. does not block, will be called multiple times
-    //if nessecary. return 0 if not done.
     virtual int init(USeconds &wokeAt);
-
     virtual int think(USeconds &wokeAt);
     virtual int sense(USeconds &wokeAt);
-
     virtual int loadConfig(ComponentConfig *conf);
-
     int readConfig(ComponentConfig *conf, string &device, int &postWait);
-
-    //cleanup
     void cleanup();
 
  private:
-    void initport(int port);
-    void closeport(int port);
+    void initport();
+    void closeport();
     void readPacket();
     
 };
