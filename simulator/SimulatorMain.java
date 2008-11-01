47f1e1deaba6790bf11e796606912a646ff938b3
marcgb <marcgb@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-11-01 01:07:27 +0000

    Added a disco interface component; SimulatorMain now looks much cleaner!

--- simulator/SimulatorMain.java
+++ simulator/SimulatorMain.java
@@ -27,18 +27,16 @@ public class SimulatorMain
     else
       subjPort = Integer.parseInt(args[1]);
 
-    System.out.println ("Creating simulator engine...");
-    final SimulatorEngine engine = new SimulatorEngine();
-    engine.addComponent(new SimulatorComponentKinematics());
-    engine.addComponent(new SimulatorComponentOmnidrive());
-
-    // Get the first agent from the engine
-    //SimulatorAgent agent = engine.getAgentList().getFirst();
-
     System.out.println ("Starting servers on ports "+objPort+" and "+subjPort);
     // Create a drop server to send and receive robot (subjective) data
     DropServer robotServ = new DropServer(subjPort);
     robotServ.start();
+
+    System.out.println ("Creating simulator engine...");
+    final SimulatorEngine engine = new SimulatorEngine();
+    engine.addComponent(new SimulatorComponentKinematics());
+    engine.addComponent(new SimulatorComponentOmnidrive());
+    engine.addComponent(new SimulatorComponentDiscoInterface(robotServ, null));
 
     javax.swing.SwingUtilities.invokeLater(new Runnable() {
         public void run() {
@@ -47,37 +45,9 @@ public class SimulatorMain
     });
 
     
-   
-    int stateThrottle = 0;
-
     while (true)
     {
       engine.step();
-      receiveControlDrops(engine, robotServ);
-
-      // Send the system state
-      // @@@ call makeDropFromState or such
-      CritterStateDrop stateDrop = new CritterStateDrop();
-      List<SimulatorAgent> agentList = engine.getAgentList();
-      SimulatorAgent varun = null;
-      if (agentList.size() > 0)
-        varun = agentList.get(0);
-
-      // @@@ NOTE TO SELF: need to synchronize with the agent list
-      if (varun != null)
-      {
-        // Wrong (this isn't acceleration!), but this is only for debugging
-        //  purposes
-        stateDrop.accel.x = 14;
-        stateDrop.accel.y = 92; 
-
-        if (++stateThrottle >= 100)
-        {
-          robotServ.sendDrop(stateDrop);
-          stateThrottle = 0;
-        }
-      }
-      else throw new RuntimeException("No agent!");
 
       try {
 		Thread.sleep(9);
@@ -88,29 +58,6 @@ public class SimulatorMain
       //objServer.sendUpdate(engine.getState());
       //subjServer.sendUpdate();
       //subjServer.receiveData();
-    }
-  }
-      
-  public static void receiveControlDrops(SimulatorEngine engine,
-    DropServer robotServ)
-  {
-    List<SimulatorDrop> drops = robotServ.receiveDrops();
-    List<SimulatorObject> drivable = 
-      engine.getObjects(SimulatorComponentOmnidrive.NAME);
-
-    // Receive some actions (@@@ needs to be written properly)
-    //  @@@ In particular, we probably want to encapsulate the setFromDrop 
-    //  code somewhere else (maybe here?)
-    for (SimulatorDrop drop : drops)
-    {
-      CritterControlDrop command = (CritterControlDrop)drop;
-      for (SimulatorObject obj : drivable)
-      {
-        ObjectStateOmnidrive driveData = 
-          (ObjectStateOmnidrive)obj.getState(SimulatorComponentOmnidrive.NAME);
-
-        driveData.setFromDrop(command);
-      }
     }
   }
 }
