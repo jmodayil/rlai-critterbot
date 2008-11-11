6aeb8b802044fdb01079e67f89909a7b8bac5b04
marcgb <marcgb@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-11-11 18:07:11 +0000

    Added the ability to draw ObjectState's If you want to disable the 'red line' in ObjectStateOmnidrive, do so by commenting out ObjectStateOmnidrive.draw (the code, but leave the method there)

--- trunk/sim/src/org/rlcommunity/critter/ObjectState.java
+++ trunk/sim/src/org/rlcommunity/critter/ObjectState.java
@@ -12,10 +12,21 @@ package org.rlcommunity.critter;
   *  @author Marc G. Bellemare
   */
 
+import java.awt.Graphics;
+
 public interface ObjectState
 {
   /** Returns a unique identifier for this type of state. */
   public String getName();
 
   public Object clone();
+
+
+  /** (Potentially) draw something about the state; may be null. This
+    *  most likely should be moved out of here when we have time.
+    *
+    * @param g The canvas to draw on
+    * @param parent The owner of this state
+    */
+  public void draw(Graphics g, SimulatorObject parent);
 }
