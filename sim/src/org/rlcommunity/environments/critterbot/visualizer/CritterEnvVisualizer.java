b858efaffa65f23f4c82ce638928a2fccaeeea79
brian@tannerpages.com <brian@tannerpages.com@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-12-05 06:51:55 +0000

    Initial support for RL-Viz visualization of the critter.  It's not as nice as the built in visualizer.

---
+++ sim/src/org/rlcommunity/environments/critterbot/visualizer/CritterEnvVisualizer.java
@@ -0,0 +1,26 @@
+package org.rlcommunity.environments.critterbot.visualizer;
+
+import rlVizLib.general.TinyGlue;
+import rlVizLib.visualization.AbstractVisualizer;
+import rlVizLib.visualization.GenericScoreComponent;
+import rlVizLib.visualization.SelfUpdatingVizComponent;
+import rlVizLib.visualization.interfaces.GlueStateProvider;
+
+public class CritterEnvVisualizer extends AbstractVisualizer implements GlueStateProvider{
+    private TinyGlue theGlueState=null;
+    
+	public CritterEnvVisualizer(TinyGlue theGlueState)  {
+		super();
+                this.theGlueState=theGlueState;
+		SelfUpdatingVizComponent theAcrobotVisualizer= new CritterImageComponent(this);
+		SelfUpdatingVizComponent theAcrobotCounter = new GenericScoreComponent(this);
+		
+		addVizComponentAtPositionWithSize(theAcrobotVisualizer,0,0,1.0,1.0);
+		addVizComponentAtPositionWithSize(theAcrobotCounter,0,0,1.0,1.0);
+	}
+
+    public TinyGlue getTheGlueState() {
+        return theGlueState;
+    }
+	
+}
