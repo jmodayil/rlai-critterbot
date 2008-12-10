546f7a34d13c080e679e3f83132d975835f82397
mike.sokolsky <mike.sokolsky@f2b56d4a-7350-0410-89f5-4fca4559de28>
2008-12-10 00:59:55 +0000

    Fancy data visualization

--- sim/src/org/rlcommunity/environments/critterbot/visualizer/CritterSensorComponent.java
+++ sim/src/org/rlcommunity/environments/critterbot/visualizer/CritterSensorComponent.java
@@ -21,7 +21,9 @@ package org.rlcommunity.environments.critterbot.visualizer;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.util.Observable;
 import java.util.Observer;
 import org.rlcommunity.rlglue.codec.types.Observation;
@@ -51,13 +53,54 @@ class CritterSensorComponent implements SelfUpdatingVizComponent, Observer {
         //Draw things here
         Observation theSensorReadings = theTinyGlue.getLastObservation();
         AffineTransform saveAT = g.getTransform();
+        AffineTransformOp op = new AffineTransformOp(
+            saveAT, AffineTransformOp.TYPE_BICUBIC);
         g.scale(.001, .001);
-        g.setFont(new Font("Serif", Font.PLAIN, 24));
-
+        g.setFont(new Font("Serif", Font.PLAIN, 50));
+        int light[] = {0, 0, 0, 0};
+        if((light[0] = theSensorReadings.intArray[40]/3) > 400)
+            light[0] = 400;
+        if((light[1] = theSensorReadings.intArray[41]/3) > 400)
+            light[1] = 400;
+        if((light[2] = theSensorReadings.intArray[42]/3) > 400)
+            light[2] = 400;
+        if((light[3] = theSensorReadings.intArray[43]/3) > 400)
+            light[3] = 400;
         g.setColor(Color.black);
-        g.drawString("Light: " + theSensorReadings.intArray[40], 0, 50);
-        g.fillRect(0, 0, 50, 50);
+        g.drawString("Light", 154, 70);
+        g.drawString("Distance", 425, 70);
+        g.drawString("Accel", 155, 570);
+        g.drawString("Rotation", 430, 570);
+        for(int i = 0; i < 4; i++) {
+            g.setColor(Color.gray);
+            g.drawRect(159 + i * 30, 99, 22, 402);
+            g.setColor(Color.yellow);
+            g.fillRect(160 + i * 30, 500 - light[i], 20, light[i]);
+        }
+        for(int i = 0; i < 10; i++) {
+            g.setColor(Color.gray);
+            g.drawRect(399 + 30 * i, 244, 22, 257);
+            g.setColor(Color.blue);
+            g.fillRect(400 + 30 * i, 500 - theSensorReadings.intArray[i + 22],
+                20, theSensorReadings.intArray[i + 22]);
+        }
+        g.setColor(Color.lightGray);
+        g.drawRect(65, 600, 300, 300);
+        g.drawOval(402, 602, 296, 296);
+        int accelX = theSensorReadings.intArray[16];
+        int accelY = theSensorReadings.intArray[17];
+        //int accelZ = theSensorReadings.intArray[18];
+        int rot = theSensorReadings.intArray[19];
+        g.setColor(Color.red);
+        g.drawLine(215, 750, 215 - accelY, 750 - accelX);
+        g.drawArc(400, 600, 300, 300, 90, (int)(rot / 5));
+        g.drawArc(404, 604, 292, 292, 90, (int)(rot / 5));
 
+        //Kernel kernel = createBlurKernel(scale);
+        //ConvolveOp op = new ConvolveOp(
+        //       kernel, ConvolveOp.EDGE_NO_OP, null);
+        //op.filter(image, tmp);
+        //op.filter(src, dst)
         g.setTransform(saveAT);
 
     }
