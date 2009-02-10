7a3cb17c1ce515ef4dfb6632e5af6d84a77139c0
whitienator <whitienator@f2b56d4a-7350-0410-89f5-4fca4559de28>
2009-02-10 22:57:25 +0000

    Multiple Battery, Multiple Charger Support

--- trunk/sim/src/org/rlcommunity/critter/SimulatorComponentBattery.java
+++ trunk/sim/src/org/rlcommunity/critter/SimulatorComponentBattery.java
@@ -4,6 +4,7 @@
  */
 package org.rlcommunity.critter;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -20,56 +21,79 @@ public class SimulatorComponentBattery implements SimulatorComponent {
 
     public void apply(SimulatorState pCurrent, SimulatorState pNext, int delta) {
 
+        LinkedList<SimulatorObject> oldBatterys = pCurrent.getObjects(ObjectStateBattery.NAME);
+        LinkedList<SimulatorObject> newBatterys = pNext.getObjects(ObjectStateBattery.NAME);        
+        int numBatterys = newBatterys.size();
+        
+        LinkedList<SimulatorObject> chargers = pCurrent.getObjects(ObjectStateBatteryCharger.NAME);
+        int numChargers = chargers.size();
+        
+        SimulatorObject oldBattery, newBattery, charger;
+        ObjectStateBatteryCharger batteryCharger;
+        ObjectStateBattery newRobotBattery, oldRobotBattery;
+        Vector2D chargerPosition, robotPosition;
+                
+        for(int iBattery=0;iBattery <numBatterys;iBattery++)
+        {
+            oldBattery = oldBatterys.get(iBattery);
+            oldRobotBattery = (ObjectStateBattery) oldBattery.getState(ObjectStateBattery.NAME);
+            robotPosition = oldBattery.getParent().getPosition();
 
-        try {
-            SimulatorObject oldBattery = pCurrent.getObjects(ObjectStateBattery.NAME).getFirst();
-            ObjectStateBattery oldRobotBattery = (ObjectStateBattery) oldBattery.getState(ObjectStateBattery.NAME);
-            Vector2D robotPosition = oldBattery.getParent().getPosition();
 
-
-            SimulatorObject newBattery = pNext.getObjects(ObjectStateBattery.NAME).getFirst();
-            ObjectStateBattery newRobotBattery = (ObjectStateBattery) newBattery.getState(ObjectStateBattery.NAME);
-
-            SimulatorObject charger = pCurrent.getObjects(ObjectStateBatteryCharger.NAME).getFirst();
-            ObjectStateBatteryCharger batteryCharger = (ObjectStateBatteryCharger) charger.getState(ObjectStateBatteryCharger.NAME);
-            Vector2D chargerPosition = charger.getPosition();
-
+            newBattery = newBatterys.get(iBattery);
+            newRobotBattery = (ObjectStateBattery) newBattery.getState(ObjectStateBattery.NAME);
+            
             newRobotBattery.setFullyCharged(false);
-
-            double dist = robotPosition.distance(chargerPosition);
+            
             int currentCharge = oldRobotBattery.getCurrentCharge();
             int newCharge = currentCharge;
             int capacity = oldRobotBattery.getChargeCapacity();
-
-            //System.out.println("level = "+ currentCharge);
+            boolean deplete = true;
             
-            if (dist < batteryCharger.getRange()) {
-                //System.out.println("charging");
-                int chargerRate = batteryCharger.getChargeRate();
-                int batteryChargeRate = newRobotBattery.getMaxChargeRate();
-                int rate;
-                if(chargerRate > batteryChargeRate)
-                    rate = batteryChargeRate; //damage battery??
-                else
-                    rate = chargerRate;
-                
-                newCharge += rate;
-                if (newCharge >= capacity) {
-                    newCharge = capacity;
-                    newRobotBattery.setFullyCharged(true);
-                }
-            } else //battery is depleted 
+            for(int jCharger=0;jCharger<numChargers;jCharger++)
             {
-                newCharge -= oldRobotBattery.getIdleDepletionRate();
-                if (newCharge < 0) {
-                    newCharge = 0; //BATTERY is Dead
+
+             charger = chargers.get(jCharger);
+             batteryCharger = (ObjectStateBatteryCharger) charger.getState(ObjectStateBatteryCharger.NAME);
+             chargerPosition = charger.getPosition();
+
+                double dist = robotPosition.distance(chargerPosition);
+
+
+                System.out.println("level = "+ currentCharge);
+            
+                if (dist < batteryCharger.getRange()) 
+                {
+                System.out.println("charging");
+                    int chargerRate = batteryCharger.getChargeRate();
+                    int batteryChargeRate = newRobotBattery.getMaxChargeRate();
+                    int rate;
+                
+                    if(chargerRate > batteryChargeRate)
+                        rate = batteryChargeRate; //damage battery??
+                    else
+                        rate = chargerRate;
+                
+                    newCharge += rate;
+                
+                    if (newCharge >= capacity) {
+                        newCharge = capacity;
+                        newRobotBattery.setFullyCharged(true);
+                    }
+                } else //battery is depleted 
+                {
+                    deplete = deplete && true;
+                
                 }
             }
-
+            if(deplete){                           
+                    newCharge -= oldRobotBattery.getIdleDepletionRate();
+                    if (newCharge < 0) 
+                        newCharge = 0; //BATTERY is Dead   
+            }
+            
             newRobotBattery.setCurrentCharge(newCharge);
-        } catch (NoSuchElementException e) {
-            //no charger and/or battery
-        }
 
+        }
     }
 }
