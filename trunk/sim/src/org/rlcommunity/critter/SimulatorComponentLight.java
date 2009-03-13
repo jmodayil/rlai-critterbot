9c2a42859a351887208328b5337e6c2fc962489d
whitienator <whitienator@f2b56d4a-7350-0410-89f5-4fca4559de28>
2009-03-13 03:02:36 +0000

    fixed bugs in light sensors

--- trunk/sim/src/org/rlcommunity/critter/SimulatorComponentLight.java
+++ trunk/sim/src/org/rlcommunity/critter/SimulatorComponentLight.java
@@ -54,6 +54,7 @@ public class SimulatorComponentLight implements SimulatorComponent {
         RayIntersection intersectData;
         Ray r;
 
+        int numDirectLightView=0;
         double dist =0;
         SimulatorObject sensor;
         ObjectStateLightSensor lightSensor;
@@ -85,29 +86,29 @@ public class SimulatorComponentLight implements SimulatorComponent {
 
             double angleBetweenRays = (Math.atan((sensorWidth / numPixels) / sensorDepth));
 
-            double currentRayAngle = 0;
-            //even or odd number of pixels..compute orientation of first ray in sensor (counter clockwise rotation from orientation of sensor).
-            if (numPixels % 2 == 0) {
-                currentRayAngle = sensor.getDirection() - angleBetweenRays / 2.0 + (Math.floor(numPixels / 2)) * angleBetweenRays;
-            } else {
-                currentRayAngle = oldSensor.getDirection() + (Math.floor(numPixels / 2)) * angleBetweenRays;            //correct if first ray rotates through PI -PI boarder
+            double currentRayAngle = 0, intialAngle;
+            
+            //ONLY odd number of pixels..compute orientation of first ray in sensor ( clockwise rotation from orientation of sensor). 
+            intialAngle = oldSensor.getDirection() + (Math.floor(numPixels / 2)) * angleBetweenRays;            //correct if first ray rotates through PI -PI boarder
+            
+            
+            if (intialAngle > Math.PI) {
+                intialAngle = -Math.PI + intialAngle - Math.PI;
             }
-            if (currentRayAngle > Math.PI) {
-                currentRayAngle = -Math.PI + currentRayAngle - Math.PI;
-            }
+            
+            currentRayAngle = intialAngle;
             double sumIntensity = 0.0; //store total intensity of each pixel in the sensor
             int maxIntensity=0;
 
             oldLightSensor.intersections = new LinkedList<RayIntersection>();
-
+            numDirectLightView = 0;
             //for each pixel in the sensor
             for (int Ipixel = 0; Ipixel < numPixels; Ipixel++) {
-                //as we rotate clockwise through pixels may cross PI -PI boarder
+                //as we rotate counterclockwise through pixels may cross PI -PI boarder
                 if (currentRayAngle < -Math.PI) {
                     currentRayAngle = Math.PI + currentRayAngle + Math.PI;                //create ray                 
                 }
                 rayDirection = Vector2D.unitVector(currentRayAngle);
-                rayDirection.normalize();
                 r = new Ray(sensorPosition, rayDirection);
 
                 //remove robot so it doesn't block ray
@@ -121,10 +122,8 @@ public class SimulatorComponentLight implements SimulatorComponent {
                 if (intersectData != null) {//if the ray hits nothing then intensity is zero. We probably have no walls
 
                     //angle between robot's sensor ray and surface normal -- old model (not used)
-                    //double angle1 = sensorPosition.minus(intersectData.point).direction();
                     
                     double angle2 = intersectData.normal.direction();
-                    //double angleOfIncidence = Math.abs(angle1) - Math.abs(angle2);
 
                     //put robot back in...robot may block light on intersection point
                     scene.addSubtree(oldSensor.getRoot());
@@ -137,8 +136,9 @@ public class SimulatorComponentLight implements SimulatorComponent {
                         lightSource = (ObjectStateLightSource) source.getState(ObjectStateLightSource.NAME);
                         srcPosition=source.getPosition();
                         
-
+                        //keep track of some sort of maximum possible light intensity
                         if(Ipixel == 0)maxIntensity +=lightSource.getIntensity();
+                        
                         //unlikely case where ray intersects the light source directly
                         double slope = (sensorPosition.x - srcPosition.x) / (sensorPosition.y - srcPosition.y);
                         double tol = Math.pow(10, -15);
@@ -151,13 +151,21 @@ public class SimulatorComponentLight implements SimulatorComponent {
                             scene.removeSubtree(source.getRoot());
 
                             //find angle between light source and surface normal
-                            double angle1 = srcPosition.minus(intersectData.point).direction();
-                            double angleOfIncidence2 = Math.abs(angle1) - Math.abs(angle2);
+                            double angle1 = -srcPosition.minus(intersectData.point).direction();
+                            double tmpAngle2 = -angle2;
+                            
+                            //convert to 2PI coordinate system
+                            if(tmpAngle2 < 0)tmpAngle2 = tmpAngle2 + 2.0*Math.PI;
+                            if(angle1 < 0)angle1 = -angle1 + 2.0*Math.PI;
+                            double angleOfIncidence2 = Math.abs(tmpAngle2 - angle1);
+                        
 
                             //is point illuminated?
                             if (scene.isVisible(srcPosition, intersectData.point)) {
                                 double dist1 = srcPosition.distance(intersectData.point); 
                                 double dist2 = intersectData.point.distance(sensorPosition);
+                                
+                                double reflectPen = 0.5;
 
                                 int distancePower = 2;
                                 
@@ -165,13 +173,40 @@ public class SimulatorComponentLight implements SimulatorComponent {
                                 //intensity =   lightSource.getIntensity() * (1.0 / Math.pow(dist1, 2)) * (1.0 / Math.pow(dist1, 2)) + Math.abs(Math.cos(angleOfIncidence) * Math.cos(angleOfIncidence2));
                                 
                                 //Lambertian reflectance model -- distance seems odd
-                               intensity = lightSource.getIntensity()*Math.abs((srcPosition.minus(intersectData.point)).dot(intersectData.normal)*Math.cos(angleOfIncidence2))*(1.0 / Math.pow(dist1, distancePower));//*(1.0 / Math.pow(dist2, distancePower)));
-                                
+                               intensity = reflectPen*lightSource.getIntensity()*Math.abs((srcPosition.minus(intersectData.point)).dot(intersectData.normal)*Math.cos(angleOfIncidence2))*(1.0 / Math.pow(dist1+dist2, distancePower));//*(1.0 / Math.pow(dist2, distancePower)));
+                                        
                                 //sum up light from multiple sources and from each pixel
                                 sumIntensity += intensity;
 
                             }
                      }
+                        if(Ipixel == numPixels-1)
+                        {
+                            scene.removeSubtree(oldSensor.getRoot());
+                               
+                            double directLightTol = Math.PI/4.0;
+                            double angleOfForwardLight = -srcPosition.minus(sensorPosition).direction();                            
+                            double sensorAngle = -sensor.getDirection();
+                                
+                            //convert to 2PI angle system for convience 
+                            if(sensorAngle < 0) sensorAngle = 2*Math.PI + sensorAngle;
+                            if(angleOfForwardLight < 0) angleOfForwardLight = 2*Math.PI + angleOfForwardLight;
+
+                                boolean nearOrigin = false;
+                                if(sensorAngle > angleOfForwardLight)
+                                {
+                                    nearOrigin = (2*Math.PI - sensorAngle + angleOfForwardLight) < directLightTol;
+                                }
+                                else
+                                    nearOrigin = (2*Math.PI - angleOfForwardLight + sensorAngle) < directLightTol;
+                                if(Math.abs(sensorAngle - angleOfForwardLight) < directLightTol || nearOrigin)
+                                {
+                                    numDirectLightView++;
+                                    intensity = lightSource.getIntensity() * (1.0 / (Math.pow(srcPosition.distance(sensorPosition), 2)));
+                                    sumIntensity += intensity;
+                                }                        
+                             scene.addSubtree(oldSensor.getRoot());
+                        }
                                 
                     }//loop over sources
                 }//test for object intersection
@@ -179,10 +214,9 @@ public class SimulatorComponentLight implements SimulatorComponent {
 
             } //loop over pixels
 
-
             //sensor reading is average of pixel readings, unless greater than sum intensity in the environment
             double noise = aRandom.nextGaussian() * lightSensor.getError();
-            double doubleReading = sumIntensity / numPixels;
+            double doubleReading = sumIntensity / (numPixels+numDirectLightView);
 
             int reading = (int)(doubleReading * (1.0 + noise));
             if(reading < 0) reading =0;
