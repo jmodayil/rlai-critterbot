c3bdbeb6d9fa3a981653c7caed0e073ccdc8593a
marcgb <marcgb@f2b56d4a-7350-0410-89f5-4fca4559de28>
2009-03-27 03:02:26 +0000

    Added data visualization within the simulator itself, based on Mike Sokolsky's visualizer

---
+++ sim/src/org/rlcommunity/critter/plugins/CritterbotDataVisualizerClient.java
@@ -0,0 +1,49 @@
+/*
+ * (c) 2009 Marc G. Bellemare.
+ */
+
+package org.rlcommunity.critter.plugins;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.rlcommunity.critter.Clients.DropClient;
+import org.rlcommunity.critter.Drops.CritterStateDrop;
+import org.rlcommunity.critter.Drops.SimulatorDrop;
+
+/** A drop client that passes the CritterStateDrops it receives on to a GUI.
+ *
+ * @author Marc G. Bellemare (mg17 at cs ualberta ca)
+ */
+public class CritterbotDataVisualizerClient implements DropClient {
+  private CritterbotVisualizerFrame aGUI;
+
+  public CritterbotDataVisualizerClient() {
+    // Create the 'GUI'
+
+    javax.swing.SwingUtilities.invokeLater(
+        new Runnable() {
+
+          public void run() {
+            aGUI = new CritterbotVisualizerFrame();
+            aGUI.setTitle("Critterbot GUI");
+            aGUI.setVisible(true);
+          }
+        });
+  }
+
+  public List<SimulatorDrop> receive() {
+    // The GUI, for now, does not generate any drops
+    return new LinkedList<SimulatorDrop>();
+  }
+
+  public void send(SimulatorDrop pData) {
+    // Parse the Critterbot data
+    if (aGUI != null && pData instanceof CritterStateDrop) {
+      aGUI.updateDisplay((CritterStateDrop) pData);
+    }
+  }
+
+  public void die() {
+    aGUI.dispose();
+  }
+}
