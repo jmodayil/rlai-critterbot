57efac602691db044174c7cf5d28b63d723a0ab4
mike.sokolsky <mike.sokolsky@f2b56d4a-7350-0410-89f5-4fca4559de28>
2009-07-04 19:34:44 +0000

    Mid-update to the power state machine

--- trunk/firmware/avr/CritterbotAVRPower/include/critterbot_power.h
+++ trunk/firmware/avr/CritterbotAVRPower/include/critterbot_power.h
@@ -10,8 +10,15 @@
 
 #define SPI_PACKET_HEADER 0x7A
 #define SPI_PADDING 0x7D
+#define CHARGE_VOLTAGE_DELAY 10
+
+#define BAT_OK 0x04
+#define CHARGE_OK 0x02
+#define VOLTAGE_OK 0x01
+
 
 extern volatile uint8_t system_voltage, rstate;
+extern uint8_t system_state;
 extern uint8_t bat40i, bat160i, bat280i;
 extern uint8_t bat40v, bat160v, bat280v;
 
