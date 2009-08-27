629662298d9e9d9f2cb0b952678954e1bc8c4fef
marcgb <marcgb@f2b56d4a-7350-0410-89f5-4fca4559de28>
2009-08-27 04:27:34 +0000

    Added a define for the not-charging state in lib_power.h Use this define in lib_motor

---
+++ firmware/arm/lib_power.h
@@ -0,0 +1,22 @@
+/*
+ * lib_power.h
+ *
+ * Created by Marc G. Bellemare 
+ * Last modified: 28 August 09
+ */
+
+#ifndef LIB_POWER_H
+#define LIB_POWER_H
+
+#define POWER_CHARGE_NOT_CHARGING  0
+#define POWER_CHARGE_40            2
+#define POWER_CHARGE_40_160        4
+#define POWER_CHARGE_160           5
+#define POWER_CHARGE_160_280       7
+#define POWER_CHARGE_280           8
+#define POWER_CHARGE_TOPOFF        9
+#define POWER_CHARGE_COMPLETE      10
+#define POWER_CHARGE_ERROR         200 
+
+#endif /* LIB_POWER_H */
+
